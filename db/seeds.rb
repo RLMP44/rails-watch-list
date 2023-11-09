ab85d58aee041430d803adab3d5e5f504ff097ba
Rachael Momber <rlmomber@gmail.com>
2023-11-09 23:41:14 +0900

    add images, need to fix delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require "open-uri"
+require "json"
+
+url = 'http://tmdb.lewagon.com/movie/popular?language=en-US&page=1'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)
+movies_list = movies['results']
+movies_list.each do |movie|
+  movie_params = {
+    title: movie['original_title'],
+    overview: movie['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500" + movie['poster_path'],
+    rating: movie['vote_average']
+  }
+  Movie.create(movie_params)
+end
