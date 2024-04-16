2f445d81e479d5819e2ae1b763d0eb0070ee7984
Rachael Momber <rlmomber@gmail.com>
2024-04-16 18:21:10 +0900

    update: models, migrations, and controllers changed to use the-great-db

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
-  has_many :bookmarks
+  self.table_name = 'wat_lis_movies'
+  has_many :bookmarks, foreign_key: 'wat_lis_movie_id'
   validates :title, uniqueness: true, presence: true
   validates :overview, presence: true
 end
