8eeec6e954cd64feb325e79737fbae7101ac0189
Rachael Momber <rlmomber@gmail.com>
2024-05-03 21:08:44 +0900

    fix: associations and validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,8 +1,8 @@
 class Bookmark < ApplicationRecord
   self.table_name = 'wat_lis_bookmarks'
-  belongs_to :movie
-  belongs_to :list
-  validates :wat_lis_movie_id, uniqueness: { scope: :list } # was :movie
+  belongs_to :movie, foreign_key: 'wat_lis_movie_id'
+  belongs_to :list, foreign_key: 'wat_lis_list_id'
+  validates :wat_lis_movie_id, uniqueness: { scope: :list, foreign_key: 'wat_lis_list_id' }
   validates :comment, length: { minimum: 6 }
 
   def movie_id
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ApplicationRecord
   self.table_name = 'wat_lis_movies'
-  has_many :bookmarks
+  has_many :bookmarks, foreign_key: 'wat_lis_movie_id'
   validates :title, uniqueness: true, presence: true
   validates :overview, presence: true
 end
